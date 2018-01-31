cbdcabca0a7cad30cceca7476710bd838838a7d4
alce65 <alce65@hotmail.es>
2018-01-31 15:16:28 +0100

    Tareas: Rx final

--- base-app/src/app/tareas/lista-rx/lista-rx.component.ts
+++ base-app/src/app/tareas/lista-rx/lista-rx.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { StoreService } from '../services/store.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-lista-rx',
@@ -11,16 +12,22 @@ export class ListaRxComponent implements OnInit {
   sTitle: string;
   sEtiquetaBtn: string;
   aItems: Array<any>;
+  aItems$: any;
+  // aItems$: Observable<any[]>;
 
   constructor(public store: StoreService) {}
 
   ngOnInit() {
     this.sTitle = 'Lista de Items';
     this.sEtiquetaBtn = 'Eliminar';
-    this.aItems = this.store.getDatos();
+    this.aItems$ = this.store.getSubjectDatos();
     // TODO: Con posterioridad a inicializar el array de tareas
     // cuando haya cambios en el store
     // debe actualizarse la lista
+    this.aItems$.subscribe(
+      (data) => this.aItems = data
+    );
+    this.store.updetaSubjectDatos();
   }
 
   deleteItem(i) {
--- base-app/src/app/tareas/services/store.service.ts
+++ base-app/src/app/tareas/services/store.service.ts
@@ -1,28 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class StoreService {
   aDatos: Array<any>;
+  aDatos$: Subject<any[]>;
 
   constructor() {
     // if (localStorage.getItem('items')) {
     //  this.aDatos = JSON.parse(localStorage.getItem('items'));
     // } else {
       this.aDatos = [];
+      this.aDatos$ = new Subject<any[]>();
     // }
    }
 
-   getDatos() {
-     return this.aDatos;
+   getSubjectDatos() {
+     return this.aDatos$;
+   }
+
+   updetaSubjectDatos() {
+    this.aDatos$.next( this.aDatos );
    }
 
    setDato(dato: any) {
     this.aDatos.push(dato);
+    // this.aDatos$.next( this.aDatos );
+    // actualizaStorage();
    }
 
    deleteDato(i: number) {
     this.aDatos.splice(i, 1);
+    // this.aDatos$.next( this.aDatos );
+    // actualizaStorage():
    }
+
 
    // private actualizaStorage() {
    // localStorage.setItem('items', JSON.stringify(this.aDatos));
